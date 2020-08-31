e919b90eb2041c4f5ef1f469aa0ad8c0fea92993
egrellis <edwardellis.egre@gmail.com>
2020-08-31 20:55:13 +0100

    Added list contents of a directory

---
+++ chapter1/ls.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <dirent.h>
+
+int
+main(int argc, char *argv[]) {
+	DIR *dp;
+	struct dirent *dirp;
+	
+	if (argc != 2) {
+		fprintf(stderr, "usage: ls directory_name\n");
+		exit(1);
+	}
+
+	if ((dp = opendir(argv[1])) == NULL) {
+		fprintf(stderr, "can't open %s\n", argv[1]);
+		exit(1);
+	}
+	while ((dirp = readdir(dp)) != NULL) {
+		printf("%s\n", dirp->d_name);
+	}
+	closedir(dp);
+	exit(0);
+}
