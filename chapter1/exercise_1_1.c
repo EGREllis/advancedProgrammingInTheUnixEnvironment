620bb6331886320fbd722ab889d70f6894c0cd8a
egrellis <edwardellis.egre@gmail.com>
2020-09-08 14:43:21 +0100

    Code recursively visits every directory using fork - must add test for . and ..

--- chapter1/exercise_1_1.c
+++ chapter1/exercise_1_1.c
@@ -12,11 +12,51 @@
 #define FILE_SEPARATOR "/"
 
 char *makeMyDirectoryName(struct dirent *dirp, char *prefix) {
-	size_t space = (size_t)(strlen(dirp->d_name) + strlen(prefix) + 1);
+	size_t space = (size_t)(strlen(dirp->d_name) + strlen(prefix) + 1 + strlen(FILE_SEPARATOR));
 	char *name = calloc(sizeof(char), space);
-	strcpy(name, FILE_SEPARATOR);
-	strcpy(name+strlen(FILE_SEPARATOR), dirp->d_name);
+	strcpy(name, prefix);
+	strcpy(name+strlen(prefix), FILE_SEPARATOR);
+	strcpy(name+strlen(prefix)+strlen(FILE_SEPARATOR), dirp->d_name);
 	return name;
+}
+
+void recursiveFileVisitor(char *directory) {
+	DIR *dp;
+	struct dirent *dirp;
+	int pid, status;
+	FILE *log = fopen("child.log", "ab");
+
+	dp = opendir(directory);
+	while ((dirp = readdir(dp)) != NULL) {
+		if (strcmp(".", dirp->d_name) == 0 || strcmp("..", dirp->d_name) == 0) {
+			continue;
+		}
+
+		char *myDirName = makeMyDirectoryName(dirp, directory);
+		fprintf(log, "Visited %s\n", myDirName);
+
+		fflush(log);
+		if ((pid = fork()) < 0) {
+			fprintf(stderr, "Fork error\n");
+			fclose(log);
+			exit(1);
+		} else if (pid == 0) {
+			// Child
+			recursiveFileVisitor(myDirName);
+			fclose(log);
+			exit(0);
+		} else {
+			// Parent
+			if ((pid = waitpid(pid, &status, 0)) == 0) {
+				fprintf(log, "Waitpid error!\n");
+				fclose(log);
+				exit(2);
+			} else if (status != 0) {
+				fprintf(log, "Waited for child process %d status %d\n", pid, status);
+			}
+		}
+	}
+	closedir(dp);
 }
 
 int main() {
@@ -27,37 +67,5 @@ int main() {
 	//	2) pwd
 	//	3) Research different methods of IPC
 
-	struct dirent *dirp;
-	DIR *dp;
-	int pid, status;
-
-	dp = opendir(ROOT_DIR);
-        while ((dirp = readdir(dp)) != NULL) {
-		char* prefix = ROOT_DIR;
-		if (strcmp(".", dirp->d_name) == 0 || strcmp("..", dirp->d_name) == 0) {
-			prefix = "";
-		}
-		fprintf(stdout, "%s%s\n", prefix, dirp->d_name);
-
-		if ((pid = fork()) < 0) {
-			fprintf(stderr, "Fork error\n");
-			exit(1);
-		} else if (pid == 0) {
-			FILE *log = fopen("child.log", "ab");
-
-			char *myDirName = makeMyDirectoryName(dirp, prefix);
-			fprintf(log, "Pid: %d\tPath: %s\n", getpid(), myDirName);
-
-			free(myDirName);
-			fclose(log);
-			exit(0);
-		} else {
-			if ((pid = waitpid(pid, &status, 0)) < 0) {
-				fprintf(stderr, "Waitpid error!\n");
-			} else if (errno != 0) {
-				fprintf(stderr, "Waited for child process %d status %d\n\terrorno: %d\terror: %s\n", pid, status, errno, strerror(errno));
-			}
-		}
-        }
-	closedir(dp);
+	recursiveFileVisitor(ROOT_DIR);
 }
